872b930ad72cc994812043de6c571c38675e04b2
grauwald <josephgray@grauwald.com>
2023-01-30 14:56:14 -0800

    more css tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { IFrame } from './components/IFrame'
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
+      <header className="app-header centered">
         <div className="app-title-image">
           <Logo />
         </div>
@@ -16,28 +16,28 @@ function App() {
           technologists who hang out, collaborate, and share ideas.
         </div>
         <div className="links">
-          Join Us: 
-          <a href="https://discord.gg/s4xZUfvp" target="_blank">Discord</a>
+          <a href="https://discord.gg/s4xZUfvp" target="_blank">Join us on Discord</a>
         </div>
       </header>
 
-      <section className="meetup">
+      <section className="meetup centered">
+
         <h2>
-          Join us for our next meetup and pop-up art show
+          Meetup and pop-up<br/>art show!
         </h2>
 
         <div class="emoji">
           💻 🎨 💾 🖌
         </div>
 
-        <div>
-          <strong>Feb 25, 2023</strong> <strong>6 pm - 10 pm</strong><br/>
-          <strong>2 + U, Downtown Seattle</strong>
+        <div class="info">
+          Feb 25, 2023 6 pm - 10 pm <br/>
+          <a href="https://goo.gl/maps/j19KQDsMy2omedUV9" target="_blank"> Studio at 2+U, Downtown Seattle</a><br/>
         </div>
 
         <div>
-          <a href="https://bit.ly/art-code-seattle" className="rsvp-button">
-            <div>RSVP on EventBrite</div>
+          <a href="https://bit.ly/art-code-seattle" className="rsvp-button copy">
+            RSVP on EventBrite
           </a>
         </div>
 
@@ -45,7 +45,7 @@ function App() {
 
       </section>
 
-      <section className="directions">
+      <section className="directions centered copy">
         <h3>Getting to the meetup</h3>
         <p>
           The venue is in downtown Seattle, and there are many bus lines that drop
@@ -56,12 +56,12 @@ function App() {
           .
         </p>
 
-        <div className="map-Container">
+        <div className="map-container">
           <IFrame
             title="map-frame"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2690.003290181105!2d-122.33673119999999!3d47.606625699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906ab183ce5671%3A0x7103eb0386ec1705!2zMitV!5e0!3m2!1sen!2sus!4v1674813641384!5m2!1sen!2sus"
-            width="600"
-            height="450"
+            width="100%"
+            height="auto"
           />
         </div>
 
